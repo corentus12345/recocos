f33a9d862b60138ae2634d3962ce48158cbac0a1
ElcocoSensei <corentin.bourgey@viacesi.fr>
2018-06-03 20:45:00 +0200

    ok

--- Lorann/model/src/main/java/model/ThreadIA.java
+++ Lorann/model/src/main/java/model/ThreadIA.java
@@ -14,8 +14,7 @@ public class ThreadIA implements Runnable{
 				if(Frame.panel.coordGhostx[x][y] != null && Frame.panel.coordGhosty[x][y] != null) {
 					
 					if(Frame.panel.getXPerso() < Frame.panel.coordGhostx[x][y] && x - 1 == Frame.panel.coordEmptyx[x - 1][y] && Frame.panel.getXPerso() != 0) {
-						
-						System.out.println(Frame.panel.getXPerso() + "   " + Frame.panel.coordGhostx[x][y] + "   " + x + "    " + y);
+
 						Frame.panel.coordGhostx[x - 1][y] = x - 1;
 						Frame.panel.coordGhosty[x - 1][y] = y;
 						
@@ -29,13 +28,14 @@ public class ThreadIA implements Runnable{
 						Frame.panel.coordEmptyx[x][y] = -1;
 						
 					}
-//					else if(Frame.panel.getXPerso() > Frame.panel.coordGhostx[x][y] && x + 1 == Frame.panel.coordEmptyx[x + 1][y]){
-//						
-//						Frame.panel.coordGhostx[x + 1][y] = x + 1;
-//						Frame.panel.coordGhosty[x + 1][y] = y;
-//						
-//						Frame.panel.coordGhostx[x][y] = null;
-//						Frame.panel.coordGhosty[x][y] = null;
+					else if(Frame.panel.getXPerso() > Frame.panel.coordGhostx[x][y] && x + 1 == Frame.panel.coordEmptyx[x + 1][y] && Frame.panel.getXPerso() != 0){
+
+						System.out.println(Frame.panel.getXPerso() + "   " + Frame.panel.coordGhostx[x][y] + "   " + x + "    " + y);
+						Frame.panel.coordGhostx[x + 1][y] = x + 1;
+						Frame.panel.coordGhosty[x + 1][y] = y;
+						
+						Frame.panel.coordGhostx[x][y] = null;
+						Frame.panel.coordGhosty[x][y] = null;
 //						
 //						Frame.panel.img[x + 1][y] = Frame.panel.img[x][y];
 //						Frame.panel.img[x][y] = Frame.panel.getImgEmpty();
@@ -43,7 +43,7 @@ public class ThreadIA implements Runnable{
 //						Frame.panel.coordEmptyx[x + 1][y] = x + 1;
 //						Frame.panel.coordEmptyx[x][y] = -1;
 //
-//					}
+					}
 				}
 			}
 		}
