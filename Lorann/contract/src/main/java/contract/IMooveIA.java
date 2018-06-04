d081300e0768eeab54bc6dff43a61399eee1af8b
corentus12345 <corentin.brion@viacesi.fr>
2018-06-04 09:30:20 +0200

    javadoc

--- Lorann/contract/src/main/java/contract/IMooveIA.java
+++ Lorann/contract/src/main/java/contract/IMooveIA.java
@@ -1,7 +1,13 @@
 package contract;
-
+/**
+ * 
+ * @author corentin BOURGEY
+ *
+ */
 public abstract interface IMooveIA{
-
+	/**
+	 * allows to move AI threads
+	 */
 	public void mooveThread();
 	
 }
